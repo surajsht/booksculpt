bc7eb8590029493e5a1a3dd2591d3db9bf8ca963
suraj <surajsht2055@gmail.com>
2024-01-21 19:05:41 +0545

    quote slider added in dashboard

---
+++ src/QuoteData.js
@@ -0,0 +1,528 @@
+export const bookQuotes = [
+  {
+    id: 1,
+    quote: "A room without books is like a body without a soul.",
+    author: "Marcus Tullius Cicero",
+  },
+  {
+    id: 2,
+    quote:
+      "The more that you read, the more things you will know. The more that you learn, the more places you'll go.",
+    author: "Dr. Seuss",
+  },
+  {
+    id: 3,
+    quote:
+      "A reader lives a thousand lives before he dies, said Jojen. The man who never reads lives only one.",
+    author: "George R.R. Martin",
+  },
+  {
+    id: 4,
+    quote: "Books are a uniquely portable magic.",
+    author: "Stephen King",
+  },
+  {
+    id: 5,
+    quote: "Reading is to the mind what exercise is to the body.",
+    author: "Joseph Addison",
+  },
+  {
+    id: 6,
+    quote:
+      "The only thing you absolutely have to know is the location of the library.",
+    author: "Albert Einstein",
+  },
+  {
+    id: 7,
+    quote: "A book is a dream that you hold in your hand.",
+    author: "Neil Gaiman",
+  },
+  {
+    id: 8,
+    quote: "There is no friend as loyal as a book.",
+    author: "Ernest Hemingway",
+  },
+  {
+    id: 9,
+    quote:
+      "You can never get a cup of tea large enough or a book long enough to suit me.",
+    author: "C.S. Lewis",
+  },
+  { id: 10, quote: "A good book has no ending.", author: "R.D. Cumming" },
+  {
+    id: 11,
+    quote: "Reading is an exercise in empathy; an adventure in self-discovery.",
+    author: "Unknown Author",
+  },
+  {
+    id: 12,
+    quote: "A well-read woman is a dangerous creature.",
+    author: "Lisa Kleypas",
+  },
+  { id: 13, quote: "The book you don't read won't help.", author: "Jim Rohn" },
+  {
+    id: 14,
+    quote:
+      "Books are mirrors: you only see in them what you already have inside you.",
+    author: "Carlos Ruiz Zafón",
+  },
+  {
+    id: 15,
+    quote:
+      "If you only read the books that everyone else is reading, you can only think what everyone else is thinking.",
+    author: "Haruki Murakami",
+  },
+  { id: 16, quote: "So many books, so little time.", author: "Frank Zappa" },
+  {
+    id: 17,
+    quote: "A book is like a garden carried in the pocket.",
+    author: "Chinese Proverb",
+  },
+  {
+    id: 18,
+    quote: "A library is not a luxury but one of the necessities of life.",
+    author: "Henry Ward Beecher",
+  },
+  {
+    id: 19,
+    quote:
+      "The man who does not read good books has no advantage over the man who cannot read them.",
+    author: "Mark Twain",
+  },
+  {
+    id: 20,
+    quote:
+      "Books are the plane, and the train, and the road. They are the destination, and the journey. They are home.",
+    author: "Anna Quindlen",
+  },
+  {
+    id: 21,
+    quote: "Reading brings us unknown friends.",
+    author: "Honoré de Balzac",
+  },
+  {
+    id: 22,
+    quote: "A room without books is like a body without a soul.",
+    author: "Marcus Tullius Cicero",
+  },
+  {
+    id: 23,
+    quote:
+      "A book is the only place in which you can examine a fragile thought without breaking it.",
+    author: "Edward P. Morgan",
+  },
+  {
+    id: 24,
+    quote:
+      "There are many little ways to enlarge your child's world. Love of books is the best of all.",
+    author: "Jacqueline Kennedy Onassis",
+  },
+  {
+    id: 25,
+    quote:
+      "To acquire the habit of reading is to construct for yourself a refuge from almost all the miseries of life.",
+    author: "W. Somerset Maugham",
+  },
+  {
+    id: 26,
+    quote:
+      "You know you've read a good book when you turn the last page and feel a little as if you have lost a friend.",
+    author: "Paul Sweeney",
+  },
+  {
+    id: 27,
+    quote:
+      "A great book should leave you with many experiences, and slightly exhausted at the end. You live several lives while reading.",
+    author: "William Styron",
+  },
+  {
+    id: 28,
+    quote:
+      "Books are the quietest and most constant of friends; they are the most accessible and wisest of counselors, and the most patient of teachers.",
+    author: "Charles William Eliot",
+  },
+  { id: 29, quote: "A good book is an event in my life.", author: "Stendhal" },
+  {
+    id: 30,
+    quote: "There is no substitute for books in the life of a child.",
+    author: "May Ellen Chase",
+  },
+  {
+    id: 31,
+    quote: "A person who won't read has no advantage over one who can't read.",
+    author: "Mark Twain",
+  },
+  {
+    id: 32,
+    quote:
+      "A great book should leave you with many experiences, and slightly exhausted at the end. You live several lives while reading.",
+    author: "William Styron",
+  },
+  {
+    id: 33,
+    quote:
+      "Books are the quietest and most constant of friends; they are the most accessible and wisest of counselors, and the most patient of teachers.",
+    author: "Charles William Eliot",
+  },
+  { id: 34, quote: "A good book is an event in my life.", author: "Stendhal" },
+  {
+    id: 35,
+    quote: "There is no substitute for books in the life of a child.",
+    author: "May Ellen Chase",
+  },
+  {
+    id: 36,
+    quote: "A person who won't read has no advantage over one who can't read.",
+    author: "Mark Twain",
+  },
+  {
+    id: 37,
+    quote: "Reading one book is like eating one potato chip.",
+    author: "Diane Duane",
+  },
+  {
+    id: 38,
+    quote:
+      "A great book should leave you with many experiences, and slightly exhausted at the end. You live several lives while reading.",
+    author: "William Styron",
+  },
+  {
+    id: 39,
+    quote:
+      "Books are the quietest and most constant of friends; they are the most accessible and wisest of counselors, and the most patient of teachers.",
+    author: "Charles William Eliot",
+  },
+  { id: 40, quote: "A good book is an event in my life.", author: "Stendhal" },
+  {
+    id: 41,
+    quote: "There is no substitute for books in the life of a child.",
+    author: "May Ellen Chase",
+  },
+  {
+    id: 42,
+    quote: "A person who won't read has no advantage over one who can't read.",
+    author: "Mark Twain",
+  },
+  {
+    id: 43,
+    quote: "Reading one book is like eating one potato chip.",
+    author: "Diane Duane",
+  },
+  {
+    id: 44,
+    quote:
+      "A great book should leave you with many experiences, and slightly exhausted at the end. You live several lives while reading.",
+    author: "William Styron",
+  },
+  {
+    id: 45,
+    quote:
+      "Books are the quietest and most constant of friends; they are the most accessible and wisest of counselors, and the most patient of teachers.",
+    author: "Charles William Eliot",
+  },
+  { id: 46, quote: "A good book is an event in my life.", author: "Stendhal" },
+  {
+    id: 47,
+    quote: "There is no substitute for books in the life of a child.",
+    author: "May Ellen Chase",
+  },
+  {
+    id: 48,
+    quote: "A person who won't read has no advantage over one who can't read.",
+    author: "Mark Twain",
+  },
+  {
+    id: 49,
+    quote: "Reading one book is like eating one potato chip.",
+    author: "Diane Duane",
+  },
+  {
+    id: 50,
+    quote:
+      "A great book should leave you with many experiences, and slightly exhausted at the end. You live several lives while reading.",
+    author: "William Styron",
+  },
+  {
+    id: 51,
+    quote: "The journey of a lifetime starts with the turning of a page.",
+    author: "Rachel Anders",
+  },
+  {
+    id: 52,
+    quote: "Reading is a discount ticket to everywhere.",
+    author: "Mary Schmich",
+  },
+  {
+    id: 53,
+    quote: "A book is a device to ignite the imagination.",
+    author: "Alan Bennett",
+  },
+  {
+    id: 54,
+    quote:
+      "In the end, we will remember not the words of our enemies, but the silence of our friends.",
+    author: "Martin Luther King Jr.",
+  },
+  {
+    id: 55,
+    quote: "Books are the mirrors of the soul.",
+    author: "Virginia Woolf",
+  },
+  {
+    id: 56,
+    quote:
+      "To read is to fly: it is to soar to a point of vantage which gives a view over wide terrains of history, human variety, ideas, shared experience, and the fruits of many inquiries.",
+    author: "A.C. Grayling",
+  },
+  {
+    id: 57,
+    quote: "A book is a garden carried in the pocket.",
+    author: "Chinese Proverb",
+  },
+  {
+    id: 58,
+    quote:
+      "The only limit to our realization of tomorrow will be our doubts of today.",
+    author: "Franklin D. Roosevelt",
+  },
+  {
+    id: 59,
+    quote:
+      "A reader lives a thousand lives before he dies. The man who never reads lives only one.",
+    author: "George R.R. Martin",
+  },
+  {
+    id: 60,
+    quote:
+      "The person who deserves most pity is a lonesome one on a rainy day who doesn’t know how to read.",
+    author: "Benjamin Franklin",
+  },
+  {
+    id: 61,
+    quote: "A good book is the purest essence of a human soul.",
+    author: "Thomas Carlyle",
+  },
+  {
+    id: 62,
+    quote:
+      "Books are the treasured wealth of the world and the fit inheritance of generations and nations.",
+    author: "Henry David Thoreau",
+  },
+  {
+    id: 63,
+    quote:
+      "I declare after all there is no enjoyment like reading! How much sooner one tires of anything than of a book!",
+    author: "Jane Austen",
+  },
+  {
+    id: 64,
+    quote:
+      "Books are the quietest and most constant of friends; they are the most accessible and wisest of counselors, and the most patient of teachers.",
+    author: "Charles William Eliot",
+  },
+  {
+    id: 65,
+    quote: "A room without books is like a body without a soul.",
+    author: "Marcus Tullius Cicero",
+  },
+  {
+    id: 66,
+    quote:
+      "The worth of a book is to be measured by what you can carry away from it.",
+    author: "James Bryce",
+  },
+  {
+    id: 67,
+    quote: "A book is a dream you hold in your hands.",
+    author: "Neil Gaiman",
+  },
+  {
+    id: 68,
+    quote:
+      "The man who does not read has no advantage over the man who cannot read.",
+    author: "Mark Twain",
+  },
+  {
+    id: 69,
+    quote:
+      "A classic is a book that has never finished saying what it has to say.",
+    author: "Italo Calvino",
+  },
+  {
+    id: 70,
+    quote: "Books are a uniquely portable magic.",
+    author: "Stephen King",
+  },
+  {
+    id: 71,
+    quote:
+      "A great book should leave you with many experiences, and slightly exhausted at the end. You live several lives while reading.",
+    author: "William Styron",
+  },
+  {
+    id: 72,
+    quote:
+      "The world is a book, and those who do not travel read only one page.",
+    author: "Saint Augustine",
+  },
+  {
+    id: 73,
+    quote:
+      "The more you read, the more things you will know. The more that you learn, the more places you'll go.",
+    author: "Dr. Seuss",
+  },
+  {
+    id: 74,
+    quote:
+      "A well-composed book is a magic carpet on which we are wafted to a world that we cannot enter in any other way.",
+    author: "Caroline Gordon",
+  },
+  {
+    id: 75,
+    quote: "The greatest gift is a passion for reading.",
+    author: "Elizabeth Hardwick",
+  },
+  {
+    id: 76,
+    quote:
+      "A mind needs books as a sword needs a whetstone, if it is to keep its edge.",
+    author: "George R.R. Martin",
+  },
+  {
+    id: 77,
+    quote:
+      "A book is the only place in which you can examine a fragile thought without breaking it.",
+    author: "Edward P. Morgan",
+  },
+  {
+    id: 78,
+    quote:
+      "There are many little ways to enlarge your child's world. Love of books is the best of all.",
+    author: "Jacqueline Kennedy Onassis",
+  },
+  {
+    id: 79,
+    quote:
+      "To acquire the habit of reading is to construct for yourself a refuge from almost all the miseries of life.",
+    author: "W. Somerset Maugham",
+  },
+  {
+    id: 80,
+    quote: "A book is a device to ignite the imagination.",
+    author: "Alan Bennett",
+  },
+  {
+    id: 81,
+    quote:
+      "In the end, we will remember not the words of our enemies, but the silence of our friends.",
+    author: "Martin Luther King Jr.",
+  },
+  {
+    id: 82,
+    quote: "Books are the mirrors of the soul.",
+    author: "Virginia Woolf",
+  },
+  {
+    id: 83,
+    quote:
+      "To read is to fly: it is to soar to a point of vantage which gives a view over wide terrains of history, human variety, ideas, shared experience, and the fruits of many inquiries.",
+    author: "A.C. Grayling",
+  },
+  {
+    id: 84,
+    quote: "A book is a garden carried in the pocket.",
+    author: "Chinese Proverb",
+  },
+  {
+    id: 85,
+    quote:
+      "The only limit to our realization of tomorrow will be our doubts of today.",
+    author: "Franklin D. Roosevelt",
+  },
+  {
+    id: 86,
+    quote:
+      "A reader lives a thousand lives before he dies. The man who never reads lives only one.",
+    author: "George R.R. Martin",
+  },
+  {
+    id: 87,
+    quote:
+      "The person who deserves most pity is a lonesome one on a rainy day who doesn’t know how to read.",
+    author: "Benjamin Franklin",
+  },
+  {
+    id: 88,
+    quote: "A good book is the purest essence of a human soul.",
+    author: "Thomas Carlyle",
+  },
+  {
+    id: 89,
+    quote:
+      "Books are the treasured wealth of the world and the fit inheritance of generations and nations.",
+    author: "Henry David Thoreau",
+  },
+  {
+    id: 90,
+    quote:
+      "I declare after all there is no enjoyment like reading! How much sooner one tires of anything than of a book!",
+    author: "Jane Austen",
+  },
+  {
+    id: 91,
+    quote:
+      "Books are the quietest and most constant of friends; they are the most accessible and wisest of counselors, and the most patient of teachers.",
+    author: "Charles William Eliot",
+  },
+  {
+    id: 92,
+    quote: "A room without books is like a body without a soul.",
+    author: "Marcus Tullius Cicero",
+  },
+  {
+    id: 93,
+    quote:
+      "The worth of a book is to be measured by what you can carry away from it.",
+    author: "James Bryce",
+  },
+  {
+    id: 94,
+    quote: "A book is a dream you hold in your hands.",
+    author: "Neil Gaiman",
+  },
+  {
+    id: 95,
+    quote:
+      "The man who does not read has no advantage over the man who cannot read.",
+    author: "Mark Twain",
+  },
+  {
+    id: 96,
+    quote:
+      "A classic is a book that has never finished saying what it has to say.",
+    author: "Italo Calvino",
+  },
+  {
+    id: 97,
+    quote: "Books are a uniquely portable magic.",
+    author: "Stephen King",
+  },
+  {
+    id: 98,
+    quote:
+      "A great book should leave you with many experiences, and slightly exhausted at the end. You live several lives while reading.",
+    author: "William Styron",
+  },
+  {
+    id: 99,
+    quote:
+      "The world is a book, and those who do not travel read only one page.",
+    author: "Saint Augustine",
+  },
+  {
+    id: 100,
+    quote:
+      "The more you read, the more things you will know. The more that you learn, the more places you'll go.",
+    author: "Dr. Seuss",
+  },
+];
--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -1,5 +1,35 @@
+import { bookQuotes } from "../QuoteData";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
+
 const Quote = () => {
-  return <div>Quote</div>;
+  return (
+    <div className="quote-container">
+      <Swiper
+        autoplay={{
+          delay: 4500,
+          disableOnInteraction: false,
+        }}
+        speed={900}
+        slidesPerView={1}
+        spaceBetween={24}
+        modules={[Autoplay]}
+      >
+        {bookQuotes.map((data) => {
+          let { id, quote, author } = data;
+          return (
+            <SwiperSlide key={id}>
+              <div className="quote-data">
+                <h2 className="quote"> {quote} </h2>
+                <span className="quote-author-name"> {`— "${author}"`} </span>
+              </div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
+  );
 };
 
 export default Quote;
